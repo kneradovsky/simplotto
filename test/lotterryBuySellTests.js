310ea54f0be5545ed2c43567718fa3a222bbc5d2
Нерадовский Константин Леонидович <kneradovsky@gmail.com>
2017-12-14 17:04:06 +0300

    tests added. Developer network configured

--- test/lotterryBuySellTests.js
+++ test/lotterryBuySellTests.js
@@ -3,10 +3,17 @@ var Simplotoken = artifacts.require("Simplotoken");
 contract("Simplotoken",function(accounts) {
    it("should put 1M on the first account",()=> {
        return Simplotoken.deployed().then((instance)=>{
-           return instance.getBalance.call(accounts[0]);
+           return instance.balanceOf(accounts[0]);
        }).then((balance)=>{
-           console.log(balance);
+
            assert.equal(balance.valueOf(),1000000,"INITIAL SUPPLY won't load to the contract account");
        })
+   })
+   it("check owner",() => {
+    return Simplotoken.deployed().then((instance)=> {
+        return instance.owner();
+    }).then(owner => {
+        assert.equal(accounts[0],owner);
+    })
    })
 })
