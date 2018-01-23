8ccbc3514e86f5b2e1c18805baf0e9af06e7ce2d
Konstantin Neradovsky <konstantin.neradovskiy@open.ru>
2018-01-23 16:00:34 +0300

    lastresult optimized

--- slsite/src/app/lastresults/lastresults.component.ts
+++ slsite/src/app/lastresults/lastresults.component.ts
@@ -7,31 +7,31 @@ import { Web3Service } from '../services/web3.service';
   styleUrls: ['./lastresults.component.css']
 })
 export class LastresultsComponent implements OnInit {
-  @Input() account : string;
-  lastGameNumber : number = 0;
-  lastTour : any = {};
-  winners : string[] = [];
-  payouts : number[] = [];
+  @Input() account: string;
+  lastGameNumber: number = 0;
+  lastTour: any = {};
+  winners: string[] = [];
+  payouts: number[] = [];
 
-  constructor(protected web3:  Web3Service,protected ref: ChangeDetectorRef) { }
+  constructor(protected web3: Web3Service, protected ref: ChangeDetectorRef) { }
 
   ngOnInit() {
     var self = this;
     this.web3.tourStarted.subscribe(event => {
-      self.updateTourData().then(()=>{});
+      self.updateTourData().then(() => { });
     })
-    if(this.account!="") this.updateTourData().then(()=>{});
+    if (this.account != "") this.updateTourData().then(() => { });
   }
 
   async ngOnChanges(changes: SimpleChanges) {
-    if(changes.account!=undefined && changes.account.currentValue!="") {
+    if (changes.account != undefined && changes.account.currentValue != "") {
       await this.updateTourData();
     }
   }
 
   protected async updateTourData() {
     this.lastGameNumber = await this.web3.SLT8.currentGameNumber() - 1;
-    if(this.lastGameNumber>=0) {
+    if (this.lastGameNumber >= 0) {
       let tourAddress = await this.web3.SLT8.prevTours(this.lastGameNumber);
       this.lastTour = this.web3.GameTourType.at(tourAddress);
       this.updateWinners(this.lastTour);
@@ -39,13 +39,15 @@ export class LastresultsComponent implements OnInit {
 
   }
 
-  protected async updateWinners(gameTour : any) {
+  protected async updateWinners(gameTour: any) {
     this.winners = await gameTour.getWinners();
     var bits = await this.web3.SLT8.bits();
-    var tickCount = Math.pow(2,bits);
-    for(var i=0;i<this.winners.length;i++) {
-      var p = await this.web3.SLT8.payouts(i);
-      this.payouts[i]=Math.ceil(p*tickCount/100);
+    var tickCount = Math.pow(2, bits);
+    if (this.payouts.length != this.winners.length) {
+      for (var i = 0; i < this.winners.length; i++) {
+        var p = await this.web3.SLT8.payouts(i);
+        this.payouts[i] = Math.ceil(p * tickCount / 100);
+      }
     }
   }
 }
